02f07c43957f285b0693077c22379716688fb0ae
clau1889 <clau.blanco1889@gmail.com>
2018-08-02 12:23:46 -0500

    En este commit se realiza función para hallar el pokémon en data

--- js/app.js
+++ js/app.js
@@ -1,12 +1,35 @@
 const getPokemon = $("#poke-input");
 const searchPokemon = $("#submit-btn");
 const putPokemon = $("#pokemon-real");
-let valuePokemon;
+let namePokemon;
 
 
 /*Función que extrae el valor de ingreso del Input*/
 searchPokemon.click( function (e){
     e.preventDefault();
     putPokemon.innerHTML="";
-    valuePokemon = getPokemon.val();
+    namePokemon = getPokemon.val();
+    console.log(namePokemon);
+
+    realPokemon();
 })
+
+const realPokemon= function (pokemon) {
+    $.ajax({
+        url: 'https://pokeapi.co/api/v2/pokemon/',
+        type:'GET',
+        datatype:'json',
+        data:{
+        name: `${namePokemon}`,   
+        }
+    })
+    .done(function(response){
+        console.log(response);
+
+    })
+    .fail(function(){
+        console.log("error");
+    })
+};
+
+
