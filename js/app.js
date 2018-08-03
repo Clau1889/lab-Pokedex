248e62812dac3e842795d514c19ad9c6845319b9
clau1889 <clau.blanco1889@gmail.com>
2018-08-02 20:39:24 -0500

    En este commit se realiza DOM para pintar en el HTML los datos del pokémon seleccionado

--- js/app.js
+++ js/app.js
@@ -3,59 +3,68 @@ const searchPokemon = $("#submit-btn");
 const putPokemon = $("#pokemon-real");
 let namePokemon;
 
+$(document).ready(function () {
+    /*Función que extrae el valor de ingreso del Input*/
+    searchPokemon.click(function (e) {
+        e.preventDefault();
+        namePokemon = getPokemon.val();
+        console.log(namePokemon);
 
-/*Función que extrae el valor de ingreso del Input*/
-searchPokemon.click( function (e){
-    e.preventDefault();
-    putPokemon.innerHTML="";
-    namePokemon = getPokemon.val();
-    console.log(namePokemon);
-
-    searchPokemonData();
-})
-
-/*Funcion para ingresar a los pokemones */
-function searchPokemonData (pokemon) {
-    $.ajax({
-        url: 'https://pokeapi.co/api/v2/pokemon/' + `${namePokemon}`,
-        type:'GET',
-        datatype:'json',
+        searchPokemonData();
     })
-    /*Función que regresa  */
-    .done(function(response){
-        const getData = (response);
-        console.log(response);
-        dataPokemon(getData);
-    })
-    .fail(function(){
-        console.log("error");
-    })
-};
 
-/*Función para meterme al Objeto de cada Pokémon y buscar propiedad */
-function dataPokemon (dataPokemon){
+    /*Funcion para ingresar a los pokemones */
+    function searchPokemonData(pokemon) {
+        $.ajax({
+            url: 'https://pokeapi.co/api/v2/pokemon/' + `${namePokemon}`,
+            type: 'GET',
+            datatype: 'json',
+        })
+            /*Función que regresa  */
+            .done(function (response) {
+                const getData = (response);
+                console.log(response);
+                dataPokemon(getData);
+            })
+            .fail(function () {
+                console.log("error");
+            })
+    };
 
-    let img= dataPokemon.sprites.front_default;
-    let name= dataPokemon.name;
-    let weight= dataPokemon.weight;
-    let abilitiesPok= dataPokemon.abilities;
+    /*Función para meterme al Objeto de cada Pokémon y buscar propiedad */
+    function dataPokemon(dataPokemon) {
+        let img = dataPokemon.sprites.front_shiny;
+        let name = dataPokemon.name;
+        let weight = dataPokemon.weight;
+        let abilitiesPok = dataPokemon.abilities;
 
-    /*Ingresar a las habilidades de cada Pokémon */
-    for (var i=0; i<abilitiesPok.length; i++){
-        let showAbilities= [];
-        let totalAbilities= abilitiesPok[i].ability.name; 
+        /*Ingresar a las habilidades de cada Pokémon*/
+        let showAbilities = [];
+        for (var i = 0; i < abilitiesPok.length; i++) {
+            let totalAbilities = abilitiesPok[i].ability.name;
 
-        showAbilities += totalAbilities;
-        console.log(showAbilities);
-    }
+            showAbilities += totalAbilities + ",";
+        };
 
+        /*USO DOM para asignar valores al pokémon seleccionado*/
+        let infoBox = $("#container-data-pokemon");
+        let pokemon = $("#pokemon-real");
+        let imgPokemon = document.createElement("img");
+        imgPokemon.src = img;
+        pokemon.append(imgPokemon);
+        infoBox.append(pokemon);
+        let textName = document.createElement('p');
+        textName.innerText = 'Name: ' + name;
+        pokemon.append(textName);
+        let textWeight = document.createElement('p');
+        textWeight.innerText = 'Weight: ' + weight;
+        pokemon.append(textWeight);
+        let textAbilities = document.createElement('p');
+        textAbilities.innerText = 'Abilities: ' + showAbilities;
+        pokemon.append(textAbilities);
 
-    let infoBox= $("#container-data-pokemon");
-    let imgPokemon= document.createElement("img");
-    imgPokemon.append(img);
-    infoBox.append(imgPokemon);
-};
+    };
 
-
+});
 
 
